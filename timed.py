ea9c5ce68468229c4d5231236a590b7e3410d63b
elliehill-hub <hill@terpmail.umd.edu>
2025-05-05 22:08:41 -0400

    adding ellies function

--- timed.py
+++ timed.py
@@ -27,3 +27,24 @@ def timed (user):
         print("You did not make it in time :/ you lose a level.")
     
 timed("player1")
+
+level1 = {"potato" : 2, 
+          "carrot" : 1, 
+          "asparagus" : 5, 
+          "broccoli" : 4, 
+          "corn" : 3
+}
+level2 = ["wheat", "flour", "bread", "biscuits"]
+level3 = ["basil", "cilantro" "dill", "parsley"]
+level4 = ["strawberry", "apple", "orange", "blueberries"]
+
+customer_satisfaction = 1
+
+def level_check_method(user_input, crops_for_sale, customer_satisfaction): 
+    bank = []
+    correct = set(user_input) & set(crops_for_sale)
+    for crop in correct:
+        bank.append(crops_for_sale[crop])
+    bank_balance = sum(bank)
+    customer_satisfaction -= len(set(user_input) ^ set(crops_for_sale))* .10
+    return (bank_balance, customer_satisfaction)
