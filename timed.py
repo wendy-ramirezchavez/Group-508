2e3b21ee46dcf58d2036018842012d0fd304d64f
elliehill-hub <hill@terpmail.umd.edu>
2025-05-06 16:09:04 -0400

    change

--- timed.py
+++ timed.py
@@ -2,6 +2,42 @@ import time
 import timeit
 import random
 
+level1 = {"potato" : 2, 
+          "carrot" : 1, 
+          "asparagus" : 5, 
+          "broccoli" : 4, 
+          "corn" : 3
+}
+level2 = {"wheat" : 2, 
+          "flour" : 1, 
+          "bread": 5, 
+          "biscuits": 4, 
+          "cake": 3}
+
+level3 = {"basil": 3, 
+          "cilantro": 2,
+          "dill": 5, 
+          "parsley": 1, 
+          "chives": 4}
+
+level4 = {"strawberry": 3, 
+          "apple": 1, 
+          "orange": 2, 
+          "blueberries": 4,
+          "raspberries": 5}
+
+customer_satisfaction = 0
+
+def level_check_method(user_input, crops_for_sale, customer_satisfaction): 
+    bank = []
+    correct = set(user_input) & set(crops_for_sale)
+    for crop in correct:
+        bank.append(crops_for_sale[crop])
+    bank_balance = sum(bank)
+    customer_satisfaction -= len(set(user_input) ^ set(crops_for_sale))* .10
+    return (bank_balance, customer_satisfaction)
+
+
 def timed (user):
     
     #test input for my function
@@ -13,7 +49,7 @@ def timed (user):
     
     #list comprehension for plushies
     for crop in crops:
-        naming = input("Enter crop {crop}")
+        naming = input("Enter crop {crop}:")
         return 
     
     after = time.time()
@@ -27,38 +63,3 @@ def timed (user):
         print("You did not make it in time :/ you lose a level.")
     
 timed("player1")
-
-level1 = {"potato" : 2, 
-          "carrot" : 1, 
-          "asparagus" : 5, 
-          "broccoli" : 4, 
-          "corn" : 3
-}
-level2 = ["wheat" : 2, 
-          "flour" : 1, 
-          "bread": 5, 
-          "biscuits": 4, 
-          "cake": 3]
-
-level3 = ["basil": 3, 
-          "cilantro": 2,
-          "dill": 5, 
-          "parsley": 1, 
-          "chives": 4]
-
-level4 = ["strawberry": 3, 
-          "apple": 1, 
-          "orange": 2, 
-          "blueberries": 4,
-          "raspberries": 5]
-
-customer_satisfaction = 1
-
-def level_check_method(user_input, crops_for_sale, customer_satisfaction): 
-    bank = []
-    correct = set(user_input) & set(crops_for_sale)
-    for crop in correct:
-        bank.append(crops_for_sale[crop])
-    bank_balance = sum(bank)
-    customer_satisfaction -= len(set(user_input) ^ set(crops_for_sale))* .10
-    return (bank_balance, customer_satisfaction)
